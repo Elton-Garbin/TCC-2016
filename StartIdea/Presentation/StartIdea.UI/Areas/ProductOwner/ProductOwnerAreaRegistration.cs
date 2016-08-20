e0e3c25d7b1a456ed40cf76e6084287c1808b61e
Elton <eltongarbin@gmail.com>
2016-08-20 18:24:33 -0300

    First

---
+++ StartIdea/Presentation/StartIdea.UI/Areas/ProductOwner/ProductOwnerAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace StartIdea.UI.Areas.ProductOwner
+{
+    public class ProductOwnerAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "ProductOwner";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "ProductOwner_default",
+                "ProductOwner/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ StartIdea/Presentation/StartIdea.UI/Areas/ScrumMaster/ScrumMasterAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace StartIdea.UI.Areas.ScrumMaster
+{
+    public class ScrumMasterAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "ScrumMaster";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "ScrumMaster_default",
+                "ScrumMaster/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
