34185f07eabe1e172ffce459b85f497c0031f47c
Elton <eltongarbin@gmail.com>
2016-08-23 00:36:18 -0300

    Inicial

---
+++ StartIdea/StartIdea.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StartIdea.UI.Startup))]
+namespace StartIdea.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
