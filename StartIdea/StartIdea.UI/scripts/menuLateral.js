4cc174be98dd65f584ff09bd303e46a7e1e99575
gabri <gabri@GABRIEL-PC>
2016-09-18 17:43:46 -0300

    Implementação menu lateral

---
+++ StartIdea/StartIdea.UI/scripts/menuLateral.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    $("#btn-menu-lateral").click(function () {
+        if ($("#menu-lateral").css("display") == "none") {
+            $("#menu-lateral").animate({ width: 'toggle' }, 350);
+        }
+    });
+
+    $("#btn-fechar-menu").click(function () {
+        if ($("#menu-lateral").css("display") == "block") {
+            $("#menu-lateral").animate({ width: 'toggle' }, 350);
+        }
+    })
+})
