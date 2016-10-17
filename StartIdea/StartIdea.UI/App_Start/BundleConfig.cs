d618511b58d398e8e1b89557b2cdbc7e83f6c678
Elton Garbin <eltongarbin@hotmail.com>
2016-10-17 20:05:19 -0200

    Client Side Validation (Without Area)

--- StartIdea/StartIdea.UI/App_Start/BundleConfig.cs
+++ StartIdea/StartIdea.UI/App_Start/BundleConfig.cs
@@ -14,16 +14,16 @@ namespace StartIdea.UI
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive-ajax.js",
                         "~/Scripts/jquery.validate-vsdoc.js",
-                        "~/Scripts/jquery.validate.js",
-                        "~/Scripts/jquery.validate.unobstrusive.js",
-                        "~/Scripts/jquery.validate.globalize.js"));
+                        "~/Scripts/jquery.validate.min.js",
+                        "~/Scripts/jquery.validate.unobtrusive.min.js",
+                        "~/Scripts/jquery.validate.globalize.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js",
+                      "~/Scripts/bootstrap.min.js",
+                      "~/Scripts/respond.min.js",
                       "~/Scripts/moment.min.js",
                       "~/Scripts/moment-with-locales.min.js",
                       "~/Scripts/bootstrap-datetimepicker.min.js"));
@@ -34,7 +34,8 @@ namespace StartIdea.UI
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.min.css",
                       "~/Content/site.css",
-                      "~/Content/bootstrap-datetimepicker.min.css"));
+                      "~/Content/bootstrap-datetimepicker.min.css",
+                      "~/Content/PagedList.css"));
         }
     }
 }
