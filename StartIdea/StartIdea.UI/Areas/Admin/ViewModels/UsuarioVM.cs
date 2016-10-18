1601fc4b1e8d9435947fa75ca67b07e8515fdda3
Gabriel Fernandes <gabrielxfds@gmail.com>
2016-10-17 22:24:47 -0200

    Validacao CPF, Validacao Email, adequação do insert e do edit

--- StartIdea/StartIdea.UI/Areas/Admin/ViewModels/UsuarioVM.cs
+++ StartIdea/StartIdea.UI/Areas/Admin/ViewModels/UsuarioVM.cs
@@ -1,5 +1,7 @@
 ﻿using PagedList;
 using StartIdea.Model;
+using StartIdea.UI.Models;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace StartIdea.UI.Areas.Admin.ViewModels
@@ -16,15 +18,19 @@ namespace StartIdea.UI.Areas.Admin.ViewModels
 
         public int Id { get; set; }
 
-        [Required, MaxLength(256)]
+        [Required(ErrorMessage = "Informe o Email"), MaxLength(256)]
+        [RegularExpression(@"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}", ErrorMessage = "Campo E-mail inválido.")]
         public string Email { get; set; }
 
-        [Required, MaxLength(256)]
+        [Required(ErrorMessage = "Informe o Apelido do usuário"), MaxLength(256)]
+        [DisplayName("Apelido")]
         public string UserName { get; set; }
 
-        [Required, StringLength(11, MinimumLength = 11)]
+        [Required(ErrorMessage = "Informe o CPF"), StringLength(11, MinimumLength = 11)]
+        [CustomValidationCPF(ErrorMessage = "CPF inválido")]
         public string CPF { get; set; }
 
+        [DisplayName("Ativo")]
         public bool IsActive { get; set; }
         #endregion
     }
