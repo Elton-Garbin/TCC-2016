f9eaf76c2446aebb193045b78183b1f282fdbe91
Elton Garbin <eltongarbin@hotmail.com>
2016-10-19 12:40:34 -0200

    Ajuste - Client Validation

--- StartIdea/StartIdea.UI/App_Start/BundleConfig.cs
+++ StartIdea/StartIdea.UI/App_Start/BundleConfig.cs
@@ -7,35 +7,35 @@ namespace StartIdea.UI
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js", 
-                        "~/Scripts/jquery-ui.min.js",
-                        "~/Scripts/globalize.js"));
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/jquery-ui.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive-ajax.js",
-                        "~/Scripts/jquery.validate-vsdoc.js",
-                        "~/Scripts/jquery.validate.min.js",
-                        "~/Scripts/jquery.validate.unobtrusive.min.js",
-                        "~/Scripts/jquery.validate.globalize.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval")
+                .Include("~/Scripts/globalize.js")
+                .Include("~/Scripts/globalize/*.js")
+                .Include("~/Scripts/jquery.validate-vsdoc.js")
+                .Include("~/Scripts/jquery.validate.js")
+                .Include("~/Scripts/jquery.validate.unobtrusive.js")
+                .Include("~/Scripts/jquery.validate.globalize.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.min.js",
-                      "~/Scripts/respond.min.js",
-                      "~/Scripts/moment.min.js",
-                      "~/Scripts/moment-with-locales.min.js",
-                      "~/Scripts/bootstrap-datetimepicker.min.js"));
+                "~/Scripts/bootstrap.min.js",
+                "~/Scripts/respond.min.js",
+                "~/Scripts/moment.min.js",
+                "~/Scripts/moment-with-locales.min.js",
+                "~/Scripts/bootstrap-datetimepicker.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/customizados").Include(
-                        "~/Scripts/masterScript.js"));
+                "~/Scripts/masterScript.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.min.css",
-                      "~/Content/site.css",
-                      "~/Content/bootstrap-datetimepicker.min.css",
-                      "~/Content/PagedList.css"));
+                "~/Content/bootstrap.min.css",
+                "~/Content/site.css",
+                "~/Content/bootstrap-datetimepicker.min.css",
+                "~/Content/PagedList.css"));
         }
     }
 }
