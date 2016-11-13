7ec99f2915489141093e7f5dc0b8b207812d550c
Gabriel Fernandes <gabrielxfds@gmail.com>
2016-11-13 17:55:29 -0200

    Tela de consulta da sprint

---
+++ StartIdea/StartIdea.UI/ViewModels/TarefaDetalhesVM.cs
@@ -0,0 +1,15 @@
+﻿using StartIdea.Model.ScrumArtefatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StartIdea.UI.ViewModels
+{
+    public class TarefaDetalhesVM
+    {
+        public int sprintId { get; set; }
+        public int productBacklogId { get; set; }
+        public Tarefa tarefa { get; set; }
+    }
+}
